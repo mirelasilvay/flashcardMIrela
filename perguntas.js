f3ee5a0152695468ceb34a3bc77b66d8a15c9561
mirelasilvay <00001084705436sp@al.educacao.sp.gov.br>
2024-11-28 18:01:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é um DNA?',
+    'e uma molecula que contem a infermacao genetica de um organismo, sendo fundamental para o seu desenvolvimento e funcionamento'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
